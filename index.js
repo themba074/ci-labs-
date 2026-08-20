7da56c703d57d68217751d7865a92fb71fe1f53a
themba074 <tsithole424@gmail.com>
2026-08-20 19:11:04 +0200

    Add multiply function

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function add(a, b) { return a + b; }
-function subtract(a, b) { return a - b; }
-module.exports = { add, subtract };
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+module.exports = {
+    add,
+    subtract,
+    multiply
+};
